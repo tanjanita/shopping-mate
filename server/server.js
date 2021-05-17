b96335f21e570e443bd1d8ad9f07b8202061514e
tanjanita <szonoko+github@gmail.com>
2021-05-17 14:43:29 +0100

    Closes #11 Link ItemListing with DB data

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
 
 const apiPort = 3333;
 const app = express();
+app.use(cors({origin: "http://localhost:3000"})); // enable api access from another origin
 app.use(express.json()); // for parsing application/json
 
 
