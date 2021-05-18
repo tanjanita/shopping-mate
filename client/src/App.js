4faef76e2379714db1d6745abaa031f269a2f26c
tanjanita <szonoko+github@gmail.com>
2021-05-18 11:42:43 +0100

    Closes #12 Set up item addition

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,19 @@
-import ItemAddition from './components/ItemAddition';
-import ItemListing from './components/ItemListing';
-import ItemDeletion from './components/ItemDeletion';
+import React from 'react';
+
+import Items from './components/Items';
 import './App.css';
 
 function App() {
+  
   return (
     <div className="App">
       <header className="App-header">
-        <h1>shoppingMate <img src="logo192.png" alt="groceries icon" width="96" height="96" /> </h1>
-        <p>Yey, you're out shopping with your mate. Let's split up that shopping list!</p>
+        <h1>shoppingMate <img src="logo192.png" alt="groceries icon" width="80" height="80" /></h1>
+        <p>Yey, you're out shopping with your mate.</p>
+        <p>Let's split up that shopping list!</p>
       </header>
 
-      <main className="App-main">
-        <ItemAddition />
-        <ItemListing />
-        <ItemDeletion />
-      </main>
+      <Items />
       
       <div className="App-footer">
         Icons made by <a href="https://www.flaticon.com/authors/monkik" title="monkik">monkik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
