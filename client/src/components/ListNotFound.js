bf0f63374ebaf24d02ae1f7219bb94cbf5ad8ea6
tanjanita <szonoko+github@gmail.com>
2021-10-29 14:34:11 +0100

    Fixes #53: Initial release final touches

--- client/src/components/ListNotFound.js
+++ client/src/components/ListNotFound.js
@@ -5,7 +5,7 @@ function ListNotFound() {
   return (
 
     <div className='itemList__empty'>
-      <p className='itemList__empty-info'>Oops, we could not find a list matching the given link/list ID.</p>
+      <p className='itemList__empty-info'><b>Oops, we could not find a list matching the given link/list ID.</b></p>
       <p className='itemList__empty-info'>Please note: Unused lists may be deleted after 90 days.</p>
       <p className='itemList__empty-info'>Please double-check your link address and try again or start a new list.</p>
     </div>
